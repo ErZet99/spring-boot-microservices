61e9e540f48d8822ecc0c18df5428465f4c5284e
ErZet99 <patryk231123@gmail.com>
2024-10-14 09:43:43 +0200

    Initialize inventory service

--- order-service/src/test/java/com/erzet99/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/erzet99/order_service/OrderServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package com.erzet99.order_service;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+import io.restassured.RestAssured;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:9.0.1");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void Setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mySQLContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+			{
+				"skuCode": "Iphone 13",
+				"price": 1000,
+				"quantity": 2
+			}
+			""";
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
 	}
 
 }
