10baf47da24396aad05cb2ef3ea6af1ebabc6f77
ErZet99 <patryk231123@gmail.com>
2024-10-22 21:13:54 +0200

    Added Open API for services and RestClient refactoring

--- order-service/src/main/java/com/erzet99/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/erzet99/order_service/client/InventoryClient.java
@@ -2,31 +2,12 @@ package com.erzet99.order_service.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.service.annotation.GetExchange;
 
-@Component
-public class InventoryClient {
-    private final RestClient restClient;
+public interface InventoryClient {
 
-    public InventoryClient(@Value("${inventory.url}/api/inventory") String uriBase) {
-        this.restClient = RestClient.builder()
-                .baseUrl(uriBase)
-                .build();
-    }
-
-    public boolean isInStock(String skuCode, Integer quantity) {
-        try {
-            return Boolean.TRUE.equals(restClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .queryParam("skuCode", skuCode)
-                            .queryParam("quantity", quantity)
-                            .build())
-                    .header("Content-Type", "application/json")
-                    .retrieve()
-                    .body(Boolean.class));
-        } catch (Exception e) {
-            System.err.println("Failed to check stock: " + e.getMessage());
-            return false;
-        }
-    }
+    @GetExchange("/api/inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
